86b13f1e0e803b1b23c162d9d9a5cc557bdddbb2
timetzhang <367271@qq.com>
2018-02-15 02:07:10 +0800

    pods, get scene list

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+target 'mind-alarm' do
+    use_frameworks!
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+end
+
--- mind-alarm/FirstViewController.swift
+++ mind-alarm/FirstViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class FirstViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var scenes:JSON = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        
+        //initialize date picker
+        self.datePicker.minimumDate = Date()
+        
+        startRequest()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func startButtonUp(_ sender: UIButton) {
+        print(self.scenes[self.pickerView.selectedRow(inComponent: 0)]["music_file"])
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.scenes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row:Int, forComponent component: Int) -> String? {
+        return "\(self.scenes[row]["name"])"
+    }
+    
+    func startRequest() {
+        Alamofire.request("http://mind-alarm-db.zhangtt.cn/getAwakeScenes").responseJSON { response in
+            if let json = response.result.value {
+                self.scenes = JSON(json)
+                self.pickerView.reloadComponent(0)
+            }
+        }
+    }
 }
 
